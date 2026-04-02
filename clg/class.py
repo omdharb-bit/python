ad0bba15f6e3b1dfcaf4ab0062ae9102a027e580
Om Dhar Badgaiyan <omdharb@gmail.com>
2026-04-02 23:13:01 +0530

    init

---
+++ clg/class.py
@@ -0,0 +1,48 @@
+# wap which will take radius of a circle as input and print and circumference of a circle
+
+# radius=float(input("Enter the radius of a circle: "))
+# circumference= 2*3.14*radius
+# print("Circumference of a circle is: ",circumference)
+
+# wap to determine grades based on marks
+# marks=int(input("Enter the marks: "))
+# if marks>=90:
+#   print("Grade A")
+# elif marks >=80:
+#     print("Grade B")
+# elif marks>=70:
+#       print("Grade C")
+# else :
+#         print("Fail")
+
+# wap to find no is divisible ny noth 3 and 5
+
+# n=int(input("Enter the no. : "))
+# if n%3==0 & n%5==0:
+#   print("no. is divisible by 3 and 5")
+# else:
+#   print("No. is not divisible ny 3 and 5")
+
+# wap to find largest of three numbers
+# a=int(input("Enter the first no. : "))
+# b=int(input("Enter the second no. : "))
+# c=int(input("Enter the third no. : "))
+# if a>b and a>c:
+#     print("Largest no. is: ",a)
+# elif b>a and b>c:
+#     print("Largest no. is: ",b)
+# else:
+#     print("Largest no. is: ",c)
+
+
+n=int(input("Enter the units. : "))
+
+if n<=50:
+    n= n*0.50
+    print("Your electricity bill: ",n)
+elif n<=150:
+    n=n*0.75
+    print("Your electricity bill: ",n)
+elif n<=250:
+    n = n * 1.20
+    print("Your electricity bill: ", n)

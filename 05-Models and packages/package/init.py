a7a9cf861c66a70f42d6dd907da83839ca53808a
Om Dhar Badgaiyan <omdharb@gmail.com>
2026-04-07 15:41:27 +0530

    hii

---
+++ 05-Models and packages/package/init.py
@@ -0,0 +1,2 @@
+'''init.py is a special Python file that is used to mark a directory as a package. It can be empty or contain initialization code for the package. In this case, we will use it to import the necessary modules for our models and packages.'''
+
---
+++ 05-Models and packages/test.py
@@ -0,0 +1,4 @@
+from package.maths import *
+
+print(addition(4,5))
+print(substraction(3,2))
